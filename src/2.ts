38af0c97d57cdc9a838f2872dd54bb8cb14c3db0
pnyekrasov <pnekrasov1910@gmail.com>
2023-11-02 01:51:45 +0200

    correct

--- src/2.ts
+++ src/2.ts
@@ -1,22 +1,7 @@
-/*
-  Ваше завдання полягатиме у створенні двох класів – Employee та Manager.
 
-  Клас Employee повинен включати:
-
-  властивість name, яка буде доступна всім.
-  властивість department, яка буде доступна лише всередині класу Employee.
-  salary, яке буде доступне лише всередині класу Employee та його підкласів.
-
-
-  Клас Manager повинен бути підклас класу Employee
-
-  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
-
-*/
 
 class Employee {
-  // Заповніть модифікатори доступу
-
+ 
   constructor(
     public name: string,
     private department: string,
@@ -29,7 +14,7 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  
   constructor(name: string, department: string, salary: number) {
     super(name, department, salary);
 
--- src/3.ts
+++ src/3.ts
@@ -1,13 +1,3 @@
-/*
-  Ви створюєте гру, де є персонажі з різними ролями.
-  Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
-
-  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
-  Інтерфейс ISpellCaster повинен включати метод castSpell.
-*/
-
-// реалізація класу Wizard
 interface ICharacter {
   name: string;
   level: number;
@@ -42,11 +32,11 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
+
 const wizard = new Wizard("Merlin", 15);
 
 wizard.introduce("I am the mighty wizard");
 wizard.castSpell();
-wizard.levelUp(); // Level up! New level is 16
+wizard.levelUp();
 
 export {};
